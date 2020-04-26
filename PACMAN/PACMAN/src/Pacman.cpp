460a59991af579550ffb712b5143e734a9887b6e
bespeso <b.espeso@alumnos.upm.es>
2020-04-26 17:51:22 +0200

    Cambios en la clase Pacman

--- PACMAN/PACMAN/src/Pacman.cpp
+++ PACMAN/PACMAN/src/Pacman.cpp
@@ -1,1 +1,38 @@
 #include "Pacman.h"
+#include "stdlib.h"
+#include "glut.h"
+
+
+// CONSTRUCTOR-DESTRUCTOR
+
+Pacman::Pacman()
+{
+
+}
+
+Pacman::~Pacman()
+{
+
+}
+
+
+// METODOS
+
+void Pacman::Dibuja()
+{
+	glPushMatrix();
+	glTranslatef(posicion.x, posicion.y + 1, 0);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glutSolidSphere(, , ); //El hombre se representa como un cubo
+	glPopMatrix();
+}
+
+void Pacman::Mueve(float t)
+{
+
+}
+
+void Pacman::SetVel(float vx, float vy)
+{
+
+}
--- PACMAN/PACMAN/src/Pacman.h
+++ PACMAN/PACMAN/src/Pacman.h
@@ -1,7 +1,24 @@
 #pragma once
+#include "Vector2D.h"
 
 class Pacman
 {
+public:
+	Pacman();
+	virtual ~Pacman();
+
+	float tamaño;
+	Vector2D posicion;
+	Vector2D velocidad;
+	Vector2D aceleracion;
+
+
+	//METODOS
+	void Dibuja();
+	void Mueve(float t);
+	void SetVel(float vx, float vy);
+	friend class Interaccion;
 
 };
+
 
