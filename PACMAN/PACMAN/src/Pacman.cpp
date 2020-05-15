f0c2ddb436e0376ffc3f80d30c45cb438ad13bc4
bespeso <b.espeso@alumnos.upm.es>
2020-05-15 19:23:19 +0200

    Simplificada la función mueve del pacman

--- PACMAN/PACMAN/src/Pacman.cpp
+++ PACMAN/PACMAN/src/Pacman.cpp
@@ -35,11 +35,8 @@ void Pacman::Mueve(float t)
 {
 	posicion.x = posicion.x + velocidad.x * t + aceleracion.x * (0.5f * t * t);
 	posicion.y = posicion.y + velocidad.y * t + 0.5f * aceleracion.y * t * t;
-	velocidad.x = velocidad.x + aceleracion.x * t;
-	velocidad.y = velocidad.y + aceleracion.y * t;
-
+	//velocidad = velocidad + aceleracion * t;
 	pacman->setPos(posicion.x,posicion.y);
-	//pacman->setVel(velocidad.x, velocidad.y);
 }
 
 void Pacman::SetVel(float vx, float vy)
