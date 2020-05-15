3c03758e401946d6b1460561d7dd33ea1401fe1b
bespeso <b.espeso@alumnos.upm.es>
2020-05-15 02:13:02 +0200

    Creada la pagina de inicio con las instrucciones del juego

--- PACMAN/PACMAN/src/CoordinadorPacman.cpp
+++ PACMAN/PACMAN/src/CoordinadorPacman.cpp
@@ -1,5 +1,5 @@
 #include "CoordinadorPacman.h"
-//#include "ETSIDI.h"
+#include "ETSIDI.h"
 #include "stdlib.h"
 #include "glut.h"
 
@@ -20,14 +20,13 @@ void CoordinadorPacman::Dibuja()
 		gluLookAt(0, 7.5, 30,  // posicion del ojo
 			0.0, 7.5, 0.0,      // hacia que punto mira  (0,0,0) 
 			0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)  
-		/*ETSIDI::setTextColor(1, 0, 1);
+		ETSIDI::setTextColor(1, 1, 0);
 		ETSIDI::setFont("fuentes/Bitwise.ttf", 16);
-		ETSIDI::printxy("Bienvenido al Pang.", -6, 10);
+		ETSIDI::printxy("Bienvenido al PACMAN.", -6, 10);
 		ETSIDI::setFont("fuentes/Bitwise.ttf", 12);
 		ETSIDI::printxy("PULSE LA TECLA -E- PARA EMPEZAR", -8, 7);
-		ETSIDI::printxy("Use los cursores izquierdo y derecho para moverse", -8, 6);
-		ETSIDI::printxy("Para disparar presione la barra espaciadora", -8, 5);
-		ETSIDI::printxy("Si quiere salir pulse la tecla -S-", -8, 4);*/
+		ETSIDI::printxy("Use los cursores izquierdo, derecho, arriba y abajo para moverse", -8, 6);
+		ETSIDI::printxy("Si quiere salir pulse la tecla -S-", -8, 5);
 	}
 	else if (estado == JUEGO)
 	{
@@ -36,17 +35,17 @@ void CoordinadorPacman::Dibuja()
 	else if (estado == GAMEOVER)
 	{
 		//mundo.Dibuja();
-		/*ETSIDI::setTextColor(1, 0, 0);
+		ETSIDI::setTextColor(1, 0, 0);
 		ETSIDI::setFont("fuentes/Bitwise.ttf", 16);
 		ETSIDI::printxy("GAMEOVER: Has perdido", -5, 10);
-		ETSIDI::printxy("Pulsa -C- para continuar", -5, 5);*/
+		ETSIDI::printxy("Pulsa -C- para continuar", -5, 5);
 	}
 	else if (estado == FIN)
 	{
 		//mundo.Dibuja();
-		/*ETSIDI::setFont("fuentes/Bitwise.ttf", 16);
+		ETSIDI::setFont("fuentes/Bitwise.ttf", 16);
 		ETSIDI::printxy("ENHORABUENA, ¡Has triunfado!", -5, 10);
-		ETSIDI::printxy("Pulsa -C- para continuar", -5, 9);*/
+		ETSIDI::printxy("Pulsa -C- para continuar", -5, 9);
 	}
 
 }
