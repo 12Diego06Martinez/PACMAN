74d0c7709e20cbbe07157d16f95c01125007a300
bespeso <b.espeso@alumnos.upm.es>
2020-05-16 15:01:47 +0200

    Creada la clase interaccion

---
+++ PACMAN/PACMAN/src/Interaccion.cpp
@@ -0,0 +1,21 @@
+#include "Interaccion.h"
+#include "ETSIDI.h"
+
+#include <iostream>
+using namespace ETSIDI;
+
+Interaccion::Interaccion()
+{
+
+}
+
+Interaccion::~Interaccion()
+{
+
+}
+
+/* bool Interaccion::colision(Pacman* p, Fantasmas h)
+{
+ Sprite* p;
+  p->
+}*/
